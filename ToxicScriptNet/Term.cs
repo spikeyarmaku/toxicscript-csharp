020244612b129315f26d6d21a3504194a7f8e890
spikeyarmaku <spikeyarmaku@gmail.com>
2023-02-15 17:57:41 +0100

    Add MultiAbs for constructing lambdas with multiple arguments

--- ToxicScriptNet/Term.cs
+++ ToxicScriptNet/Term.cs
@@ -2,6 +2,36 @@ namespace ToxicScriptNet;
 
 public abstract class Term<T> {
     public abstract override string ToString();
+
+    public static Term<T> MultiAbs(
+            int paramCount,
+            Func<Env<Term<T>>,
+            List<Expr>,
+            Term<T>> body) {
+        List<Expr> exprs = new List<Expr>();
+        return MultiAbsHelper(exprs, paramCount, body);
+    }
+
+    private static Term<T> MultiAbsHelper(
+            List<Expr> exprs,
+            int paramCount,
+            Func<Env<Term<T>>,
+            List<Expr>,
+            Term<T>> body) {
+        Term<T> term;
+        if (paramCount == 1) {
+            term = new Abs<T>((env, p) => {
+                exprs.Add(p);
+                return body(env, exprs);
+            });
+        } else {
+            term = new Abs<T>((_, p) => {
+                exprs.Add(p);
+                return MultiAbsHelper(exprs, paramCount - 1, body);
+            });
+        }
+        return term;
+    }
 }
 
 public class Abs<T> : Term<T> {
